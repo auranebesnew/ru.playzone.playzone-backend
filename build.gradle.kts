6cde5c7df49ac60b7342653893bf7401747986e4
Arslan Abdullaev <ars19990102@gmail.com>
2024-03-11 22:45:35 +0300

    added gradle stage task

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,13 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
 
     implementation("org.postgresql:postgresql:42.2.2")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
 
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+tasks.create("stage") {
+    dependsOn("installDist")
 }
--- src/main/kotlin/ru/playzone/Application.kt
+++ src/main/kotlin/ru/playzone/Application.kt
@@ -3,6 +3,7 @@ package ru.playzone
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.Database
 import ru.playzone.features.games.configureGamesRouting
@@ -11,9 +12,9 @@ import ru.playzone.features.register.configureRegisterRouting
 import ru.playzone.plugins.*
 
 fun main() {
-    Database.connect("jdbc:postgresql://localhost:5432/arslan.a", driver = "org.postgresql.Driver", "arslan.a", password = "123456")
+//    Database.connect("jdbc:postgresql://localhost:5432/arslan.a", driver = "org.postgresql.Driver", "arslan.a", password = "123456")
 
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = System.getenv("PORT").toInt(), module = Application::module)
         .start(wait = true)
 }
 
